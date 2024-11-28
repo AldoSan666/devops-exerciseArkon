018581c414ef674b51d18e0527ecee3c7dd123dd
Aldo Sanchez <aldosan666@outlook.com>
2024-11-28 16:12:37 -0600

    Added counter
    And BR for sepparation

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+post_counter = 0
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return "Hello Cruel World!!!"
-
+    global post_counter
+    if request.method == 'POST':
+        post_counter += 1
+    return f"Hello Cruel World!!!<br>POST requests count: {post_counter}"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)

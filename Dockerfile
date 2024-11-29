fb31c6e4c5fb7f2c69063edb20d4472431ee9daa
Aldo Sanchez <aldosan666@outlook.com>
2024-11-28 18:43:22 -0600

    Docker publish
    changed name and removed secrets name HUB word

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 
 FROM python:3.9-slim
 WORKDIR /app
+COPY requirement.txt requirement.txt
+COPY app.py .
+COPY . . 
 COPY "requirement.txt" .
 RUN pip install -r requirement.txt
-
-COPY app.py .
 EXPOSE 5000
 CMD ["python","app.py"]
--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, request
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
-post_counter = 0
+app = Flask(__name__, template_folder='templates')
+post_count = 0  
 
-@app.route("/", methods=['GET', 'POST'])
-def hello():
-    global post_counter
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global post_count
     if request.method == 'POST':
-        post_counter += 1
-    return f"Hello Cruel World!!!<br>POST requests count: {post_counter}"
+        if 'reset' in request.form:
+            post_count = 0
+        else:
+            post_count += 1
+    return render_template('index.html', post_count=post_count)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)

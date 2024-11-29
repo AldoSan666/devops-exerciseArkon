002a918acdc48cf19842cd519d952cf780a1b328
Aldo Sanchez <aldosan666@outlook.com>
2024-11-28 19:41:52 -0600

    Added GET
    And JSON format

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__, template_folder='templates')
 post_count = 0  
@@ -12,6 +12,16 @@ def index():
         else:
             post_count += 1
     return render_template('index.html', post_count=post_count)
+
+@app.route('/post', methods=['POST']) 
+def handle_post(): 
+    global post_count 
+    post_count += 1 
+    return "POST request received", 200
+
+@app.route('/get', methods=['GET']) 
+def handle_get():
+    return jsonify({"post_count": post_count})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
